5b4d5934e50dddfeb930e9d12490e6702252b6c7
Daniel Zazzali <dazazzali@gmail.com>
2024-12-27 16:18:04 -0300

    Updated

--- index.js
+++ index.js
@@ -1,41 +1,11 @@
 import { calculateMetrics } from '../metrics/index.js';
 
-
-const metricsMOCK = [
-    {
-        name: "MOCK",
-        path: "MOCK.js"
-    }
-]
-
 async function run() {
 
-    const path = "./src";
+    const codePath = "./src";
+    const useDefaultMetrics = true;
 
-    const metrics = [
-        {
-            name: "Function Declaration",
-            path: "/home/daniel/Workspace/metrics/src/metrics/functionDeclaration.js",
-        },
-        {
-            name: "Classes Per File",
-            path: "/home/daniel/Workspace/metrics/src/metrics/classesPerFile.js"
-        },
-        {
-            name: "Methods Per File",
-            path: "/home/daniel/Workspace/metrics/src/metrics/methodsPerFile.js"
-        },
-        {
-            name: "Fan In Fan Out Per File",
-            path: "/home/daniel/Workspace/metrics/src/metrics/fanInFanOutPerFile.js"
-        },
-        {
-            name: "Fan In Fan Out Per Class",
-            path: "/home/daniel/Workspace/metrics/src/metrics/fanInFanOutPerClass.js"
-        }
-    ]
-
-    const results = await calculateMetrics({path});
+    const results = await calculateMetrics({codePath, useDefaultMetrics});
     console.log(JSON.stringify(results, null, 2));
 }
 
