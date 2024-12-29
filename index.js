fa234e759f5464ed6b2a2f3d35c7e1a6902534fd
Daniel Zazzali <dazazzali@gmail.com>
2024-12-29 18:00:32 -0300

    Added custom metric example

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import { calculateMetrics } from 'metrics-wizard';
 (async () => {
     const results = await calculateMetrics({
         codePath: './src',
-        //customMetricsPath: './metrics',
+        customMetricsPath: './metrics',
         useDefaultMetrics: true
     });
 
     results.forEach(result => {
-        console.log('\x1b[36m%s\x1b[0m', '------------------------------------');
+        console.log('\x1b[36m%s\x1b[0m', "-".repeat(80));
         console.log('\x1b[32m%s\x1b[0m', result.metricName);
         console.log('\x1b[32m%s\x1b[0m', result.description);
         console.log('\x1b[32m%s\x1b[0m', `Version: ${result.version}`);
